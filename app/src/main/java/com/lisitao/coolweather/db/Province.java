1b5cbc87210bc77a972f0803d30548b954e1ed89
cangjietai <1296172553@qq.com>
2022-03-30 15:40:32 +0800

    加入创建数据库和表的各项设置

---
+++ app/src/main/java/com/lisitao/coolweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.lisitao.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+//访问全国的省份：http://guolin.tech/api/china
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
